a69a03462b90199a8e60140e80bfa14c42660a75
stonasss <luis.b.santos96@gmail.com>
2023-04-09 22:02:43 -0300

    feat: added delete & get user controllers

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,39 +1,52 @@
 import { userServices } from "../services/user-services.js"
 import { Request, Response, NextFunction } from "express";
+import { VerifyId, LogInUser, RegisterUser } from "../protocols/users.js";
 
 async function signUp(req: Request, res: Response, next: NextFunction){
-    const { name, email, password } = req.body;
+    const { name, email, password } = req.body as RegisterUser;
 
     try {
         await userServices.createUser({ name, email, password });
         return res.sendStatus(201);
     } catch (err) {
         next(err);
-    }
-}
+    };
+};
 
 async function logIn(req: Request, res: Response, next: NextFunction){
-    const { email, password } = req.body;
+    const { email, password } = req.body as LogInUser;
 
     try {
         const token = await userServices.updateUser({ email, password });
-        return res.send({ token })
+        return res.send({ token });
     } catch (err) {
         next(err);
-    }
-}
+    };
+};
 
-async function getUsers(){
+async function getUsers(req: Request, res: Response, next: NextFunction){
+    try {
+        const users = await userServices.findUsers();
+        return res.send({ users });
+    } catch (err) {
+        next(err);
+    };
+};
 
-}
+async function deleteUser(req: Request, res: Response, next: NextFunction){
+    const { id } = req.body as VerifyId;
 
-async function deleteUser(){
-
-}
+    try {
+        await userServices.deleteUser(id);
+        return res.sendStatus(204);
+    } catch (err) {
+        next(err);
+    };
+};
 
 export const userControllers = {
     signUp,
     logIn,
     getUsers,
     deleteUser,
-}
+};
