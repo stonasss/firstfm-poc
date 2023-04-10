100620a9eabb52eed4532fefa272ade5bc2d5712
stonasss <luis.b.santos96@gmail.com>
2023-04-09 22:09:29 -0300

    feat: user routes applied

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from "./routes/index.js";
+import { handleApplicationErrors } from "./middlewares/error-middlewares.js";
 
 dotenv.config();
+
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.use(router);
+server.use(handleApplicationErrors);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,1 +1,11 @@
 import { Router } from "express";
+import { userControllers } from "../controllers/user-controllers.js";
+
+const userRoutes = Router();
+
+userRoutes.post("/sign-up", userControllers.signUp);
+userRoutes.get("/users", userControllers.getUsers);
+userRoutes.put("/log-in", userControllers.logIn);
+userRoutes.delete("/delete-user", userControllers.deleteUser);
+
+export default userRoutes;
