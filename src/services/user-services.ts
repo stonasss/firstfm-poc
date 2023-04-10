45b74799d3f88f046455921b1f9fba3fd3a6702f
stonasss <luis.b.santos96@gmail.com>
2023-04-09 21:02:39 -0300

    feat: read & delete users

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -5,16 +5,16 @@ import { userRepositories } from "../repositories/user-repositories.js";
 import { errors } from "../errors/index.js";
 import { RegisterUser, LogInUser } from "../protocols/users.js";
 
-async function update({ email, password }: LogInUser) {
+async function updateUser({ email, password }: LogInUser) {
     const { 
         rowCount, 
         rows: [user],
     } = await userRepositories.findByEmail(email);
+
     if (!rowCount) throw errors.invalidCredentialsError(email);
-
     const correctPasswd = bcrypt.compare(password, user.password);
-    if (!correctPasswd) throw errors.invalidCredentialsError(email);
 
+    if (!correctPasswd) throw errors.invalidCredentialsError(email);
     if (!user.token) {    
         const token = jwt.sign({ user_id: user.id }, process.env.SECRET_KEY);
         await userRepositories.loginUser(token, user.id);
@@ -22,10 +22,10 @@ async function update({ email, password }: LogInUser) {
     }
 }
 
-async function create({ name, email, password }: RegisterUser) {
+async function createUser({ name, email, password }: RegisterUser) {
     const {rowCount} = await userRepositories.findByEmail(email);
-    if (rowCount) throw errors.duplicatedEmail(email);
 
+    if (rowCount) throw errors.duplicatedEmail(email);
     const hashedPasswd: string = await bcrypt.hash(password, 10);
     await userRepositories.createUser({
         name,
@@ -34,7 +34,23 @@ async function create({ name, email, password }: RegisterUser) {
     });
 }
 
+async function findUsers() {
+    const { rows, rowCount } = await userRepositories.getUsers();
+
+    if (!rowCount) throw errors.notFoundError();
+    return rows;
+}
+
+async function deleteUser(id: number) {
+    const { rowCount } = await userRepositories.findById(id);
+
+    if (!rowCount) throw errors.notFoundError();
+    await userRepositories.deleteUser(id);
+}
+
 export const userServices = {
-    create,
-    update,
+    createUser,
+    updateUser,
+    findUsers,
+    deleteUser,
 }
