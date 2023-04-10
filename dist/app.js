57abd021201e3795b567405aae6ea22901242375
Luis Bernardo <102603174+stonasss@users.noreply.github.com>
2023-04-09 22:14:23 -0300

    patch: remove dist directory & add to .gitignore

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
-var server = express();
-server.use(cors());
-server.use(express.json());
-var port = process.env.PORT || 5000;
-server.listen(port, function () { return console.log("Server running on port ".concat(port)); });
